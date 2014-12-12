d512fb64e850d2b71450866ab6b6456f2a9ed06d
David Fedele <dave.fedele@gmail.com>
2014-12-12 15:38:21 -0800

    implement graph - store nodes

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,26 @@
 
-
 var Graph = function(){
+  this.storage = {};
+  // edges
+  // how to store edges
+  // object with key of node name and value of array of edge names
 };
 
 Graph.prototype.addNode = function(node){
+  this.storage[node] = [];
+  // Put node in graph
+  // add edges
+  // add complementary edge
 };
 
 Graph.prototype.contains = function(node){
+  return node in this.storage;
 };
 
 Graph.prototype.removeNode = function(node){
+  // remove node from graph
+  // remove edges
+  // remove complementary edges
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
