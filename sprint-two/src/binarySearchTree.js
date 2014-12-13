2d0d734dcf48373dcb47b1bd1dc68fc8a7059a0d
David Fedele <dave.fedele@gmail.com>
2014-12-12 19:08:52 -0800

    implement binary search tree - add methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,35 @@
 var BinarySearchTree = function(value){
+  // Storage data structure
+  // Object with left and right
+  var obj = Object.create(BinarySearchTree.prototype);
+  obj.storage = value;
+  obj.left;
+  obj.right;
+  return obj;
 };
 
+BinarySearchTree.prototype = {};
+
+BinarySearchTree.prototype.insert = function(value) {
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  
+};
+// Insert: compare value to current node value
+// If less than node value, new value placed as left child.
+// As right child if greater than.
+
+/* Contains: iterate through tree and return boolean
+*/
+
+/* depthfirstlog: iterate through values from top down and execute callback.
+*/
 
 /*
  * Complexity: What is the time complexity of the above functions?
