90cb2cc6a34f4f18d67e614105d1b7d972ad626a
David Fedele <dave.fedele@gmail.com>
2014-12-13 11:49:04 -0800

    extra credit - tests

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -15,6 +15,7 @@ describe('tree', function() {
   it('should have properties named "value" and "parent".', function () {
     expect(tree.hasOwnProperty("value")).to.equal(true);
     expect(tree.hasOwnProperty("parent")).to.equal(true);
+    expect(tree.parent).to.be.null;
   });
 
   it('should add children to the tree', function() {
@@ -50,23 +51,42 @@ describe('tree', function() {
   describe('Parent', function () {
     it('should be referenced for all child nodes.', function () {
       tree.addChild(1);
-      expect(tree.children[0].value).to.equal(tree.children[0].parent.value);
+      expect(tree.value).to.equal(tree.children[0].parent.value);
       tree.addChild(2);
-      expect(tree.children[1].value).to.equal(tree.children[1].parent.value);
+      expect(tree.value).to.equal(tree.children[1].parent.value);
       tree.addChild(3);
-      expect(tree.children[2].value).to.equal(tree.children[2].parent.value);
+      expect(tree.value).to.equal(tree.children[2].parent.value);
       tree.children[0].addChild(4);
-      expect(tree.children[0].children[0].value).to.equal(tree.children[0].children[0].parent.value);
+      expect(tree.children[0].value).to.equal(tree.children[0].children[0].parent.value);
       tree.children[1].addChild(5);
-      expect(tree.children[1].children[0].value).to.equal(tree.children[1].children[0].parent.value);
+      expect(tree.children[1].value).to.equal(tree.children[1].children[0].parent.value);
       tree.children[2].addChild(6);
-      expect(tree.children[2].children[0].value).to.equal(tree.children[2].children[0].parent.value);
+      expect(tree.children[2].value).to.equal(tree.children[2].children[0].parent.value);
     });
 
     it('should bilaterally detach and return a sub-tree from its parent.', function () {
+      tree.addChild(1);
+      tree.addChild(2);
+      tree.children[0].addChild(3);
+      tree.children[0].addChild(4);
+      var node = tree.children[0].removeFromParent();
+      expect(node.parent).to.be.null;
+      expect(node.value).to.equal(1);
+      // xexpect(node.children[0]value).to.equal(3);
+      // xexpect(node.children[0]value).to.equal(4);
+    });
 
+    xit('should execute a callback on each node of the tree.', function() {
+      var array = [];
+      var func = function(value) {array.push(value);};
+      tree.addChild(1);
+      tree.addChild(2);
+      tree.children[0].addChild(3);
+      tree.children[0].addChild(4);
+      tree.children[1].addChild(13);
+      tree.children[1].addChild(14);
+      tree.traverse(func);
+      expect(array).to.eql([1, 3, 4, 2, 13, 14]);
     });
   });
-
-
 });
