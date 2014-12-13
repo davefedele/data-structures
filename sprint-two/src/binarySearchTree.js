daee5e6b034149b197112a84f457e0488afaf4c6
David Fedele <dave.fedele@gmail.com>
2014-12-12 21:16:45 -0800

    implement binary search tree - store value and contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -2,7 +2,7 @@ var BinarySearchTree = function(value){
   // Storage data structure
   // Object with left and right
   var obj = Object.create(BinarySearchTree.prototype);
-  obj.storage = value;
+  obj.value = value;
   obj.left;
   obj.right;
   return obj;
@@ -11,25 +11,59 @@ var BinarySearchTree = function(value){
 BinarySearchTree.prototype = {};
 
 BinarySearchTree.prototype.insert = function(value) {
-
+  if (this.value !== value){
+    if (value > this.value) {
+      if( this.right === undefined) {
+        var newNode = BinarySearchTree(value);
+        this.right = newNode;
+      }
+      else {
+        this.right.insert(value);
+      }
+    }
+    else {
+      if (this.left === undefined) {
+        var newNode = BinarySearchTree(value);
+        this.left = newNode;
+      }
+      else {
+        this.left.insert(value);
+      }
+    }
+  } 
 };
 
 BinarySearchTree.prototype.contains = function(value) {
-  
+/* Contains: iterate through tree and return boolean
+*/
+  if (this.value === value) {
+    return true;
+  }
+  else {
+    if (value > this.value) {
+      if (this.right) {
+        if (this.right.contains(value) === true) {
+          return true;
+        }
+      }
+    }
+    else {
+      if (this.left) {
+        if (this.left.contains(value) === true) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
-  
-};
-// Insert: compare value to current node value
-// If less than node value, new value placed as left child.
-// As right child if greater than.
-
-/* Contains: iterate through tree and return boolean
-*/
-
 /* depthfirstlog: iterate through values from top down and execute callback.
 */
+};
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
